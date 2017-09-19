fefcf76ae4b5f93439467ec491e781c4bd5160f4
Zifan94 <mikeYang1625@gmail.com>
2017-09-18 22:15:01 -0400

    Submitting submission.py lab 1e

--- lab_1e/PassThroughProtocol.py
+++ lab_1e/PassThroughProtocol.py
@@ -6,61 +6,61 @@ from VerificationCodeClientProtocol import *
 import playground
 
 # Pass Through Server
-class PassThroughServerProtocol(StackingProtocol):
+class PassThroughProtocol1(StackingProtocol):
 	def __init__(self):
-		print("[Server Pass Through]: Init Compelete...")
+		print("[Pass Through 1]: Init Compelete...")
 		super().__init__
 		self.transport = None
 
 	def connection_made(self, transport):
-		print("[Server Pass Through]: Connection Made...")
+		print("[Pass Through 1]: Connection Made...")
 		self.transport = transport
 		higherTransport = StackingTransport(self.transport)
 		self.higherProtocol().connection_made(higherTransport)
 
 	def connection_lost(self, exc=None):
-		# print("[Server Pass Throught]: Connection Losting...")
+		# print("[Pass Through 1]: Connection Losting...")
 		self.higherProtocol().connection_lost()
 		self.transport = None
-		print("[Server Pass Throught]: Connection Lost...")
+		print("[Pass Through 1]: Connection Lost...")
 
 	def data_received(self, data):
-		#print("[Server Pass Through]: data received...%s"%data)
-		print("[Server Pass Through]: data received...")
+		#print("[Pass Through 1]: data received...%s"%data)
+		print("[Pass Through 1]: data received...")
 		self.higherProtocol().data_received(data)
-		if self.higherProtocol().state == "close_state":
+		if self.higherProtocol().state == "close_state" or self.higherProtocol().state == "finish_state":
 			self.transport.close()
 
 # Pass Through Client
-class PassThroughClientProtocol(StackingProtocol):
+class PassThroughProtocol2(StackingProtocol):
 	def __init__(self):
-		print("[Client Pass Through]: Init Compelete...")
+		print("[Pass Through 2]: Init Compelete...")
 		super().__init__
 		self.transport = None
 
 	def connection_made(self, transport):
-		print("[Client Pass Through]: Connection Made...")
+		print("[Pass Through 2]: Connection Made...")
 		self.transport = transport
 		higherTransport = StackingTransport(self.transport)
 		self.higherProtocol().connection_made(higherTransport)
 
 	def connection_lost(self, exc=None):
-		# print("[Client Pass Through]: Connection Losting...")
+		# print("[Pass Through 2]: Connection Losting...")
 		self.higherProtocol().connection_lost()
 		self.transport = None
-		print("[Client Pass Through]: Connection Lost...")
+		print("[Pass Through 2]: Connection Lost...")
 
 	def data_received(self, data):
-		# print("[Client Pass Through]: data received...%s"%data)
-		print("[Client Pass Through]: data received...")
+		# print("[Pass Through 2]: data received...%s"%data)
+		print("[Pass Through 2]: data received...")
 		self.higherProtocol().data_received(data)
-		if self.higherProtocol().state == "finish_state":
+		if self.higherProtocol().state == "close_state" or self.higherProtocol().state == "finish_state":
 			self.transport.close()
 
 
 
 
-f = StackingProtocolFactory(lambda: PassThroughServerProtocol(), lambda: PassThroughClientProtocol())
+f = StackingProtocolFactory(lambda: PassThroughProtocol1(), lambda: PassThroughProtocol2())
 ptConnector = playground.Connector(protocolStack=f)
 playground.setConnector("passthrough", ptConnector)
 print("----- NEW CONNECTOR passthrough SETUP -----")
